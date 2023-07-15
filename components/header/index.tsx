95fa9b280f81b165a8f623bb9aa216abb2644955
yuta-akamine <happykaizokuou@gmail.com>
2023-07-15 15:55:36 +0900

    🎨 ヘッダーコンポーネント制作

---
+++ components/header/index.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { Box, Grid, Typography } from '@mui/material'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const header = () => {
+
+  return (
+    
+    <Box>
+
+      <header>
+
+        <nav>
+
+          <h1 className="maintitle nounderline topspace bottomspace">
+            <a href="index.php" className="">Akamine's Website</a>
+          </h1>
+
+          <Grid container spacing={5}>
+
+            <Grid item xs={3}>
+              <a href="index.php">Home</a>
+            </Grid>
+
+            <Grid item xs={3}>
+              <a href="profile.php">Profile</a>
+            </Grid>
+
+            <Grid item xs={3}>
+              <a href="development.php">Development</a>
+            </Grid>
+
+            <Grid item xs={3}>
+              <a href="skills.php">Skills</a>
+            </Grid>
+
+          </Grid>
+
+        </nav>
+
+      </header>
+
+      <hr  className="bottomspace" />
+
+    </Box>
+
+  );
+
+}
+
+export default header;
